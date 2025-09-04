0aed6af4f2ee540b10db55946ed5135af34f630f
Thiago Kufa <tkufa.code@gmail.com>
2025-09-04 20:58:03 -0300

    feat(dto): add product DTO schema and types

---
+++ src/dto/productDTO.ts
@@ -0,0 +1,26 @@
+import z from 'zod'
+
+// model Produto {
+//   id           Int      @id @default(autoincrement())
+//   nome         String
+//   preço        Float
+//   estoque      Int
+//   data_criacao DateTime @default(now())
+// }
+const productSchema = z.object({
+  productId: z.number().optional(),
+  name: z.string(),
+  price: z.number(),
+  stock: z.number(),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+})
+
+export const createProductSchema = productSchema.omit({productId: true, createdAt: true})
+export const updateProductSchema = productSchema.omit({productId: true, createdAt: true, updatedAt: true})
+
+export type ProductDTO = z.infer<typeof productSchema>
+export type CreateProductDTO = z.infer<typeof createProductSchema>
+export type UpdateProductDTO = z.infer<typeof updateProductSchema>
+
+
