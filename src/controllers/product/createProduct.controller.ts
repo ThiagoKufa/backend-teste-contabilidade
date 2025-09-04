d660eff47710b36a6c61ee6626c13a91c80535fb
Thiago Kufa <tkufa.code@gmail.com>
2025-09-04 20:57:29 -0300

    feat(product): add create product controller with validation

---
+++ src/controllers/product/createProduct.controller.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from 'express'
+import { createProductSchema } from '../../dto/productDTO'
+import z from 'zod'
+import { createProductService } from '../../service/product/createProduct.service'
+
+
+
+export const createProductController = async (req: Request, res: Response) => {
+  const {name, price, stock} = req.body
+  try {
+    const createProductDTO = createProductSchema.parse({name, price, stock})
+    const product = await createProductService(createProductDTO)
+    return res.status(201).json({
+      message: 'Produto criado com sucesso',
+      data: product
+    })
+  } catch (error) {
+    if(error instanceof z.ZodError){
+      return res.status(400).json(error.issues)
+    }
+  }
+}
