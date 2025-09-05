4d072d721064321552135a295ccb79eb40f01ab6
Thiago Kufa <tkufa.code@gmail.com>
2025-09-05 10:41:10 -0300

    feat(product): add endpoints to list products and get by id
    Add GET endpoints to retrieve all products and a specific product by ID to complete CRUD functionality

--- src/router/product.router.ts
+++ src/router/product.router.ts
@@ -1,8 +1,12 @@
 import {Router} from 'express'
 import {createProductController} from '../controller/product/createProduct.controller'
+import {listProductsController} from '../controller/product/listProducts.controller'
+import {getProductByIdController} from '../controller/product/getProductById.controller'
 
 const productRouter = Router()
 
 productRouter.post('/produtos', createProductController)
+productRouter.get('/produtos', listProductsController)
+productRouter.get('/produtos/:id', getProductByIdController)
 
 export default productRouter
